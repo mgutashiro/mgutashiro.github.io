8571b69a2aa60cd11d819ba83bef56d8d4f29f4f
Monica Aichouri <69013706+mgu0625@users.noreply.github.com>
2025-11-15 21:45:23 -0500

    don't remember edit

---
+++ src/Experience/Experience.js
@@ -0,0 +1,98 @@
+import * as THREE from 'three'
+import Sizes from './Utils/Sizes.js'
+import Time from './Utils/Time.js'
+import Camera from './Camera.js'
+import Renderer from './Renderer.js'
+import World from './World/World.js'
+import Resources from './Utils/Resources.js'
+import Debug from './Utils/Debug.js'
+import sources from './sources.js'
+
+let instance = null
+
+export default class Experience
+{
+    constructor(canvas)
+    {
+        if(instance)
+        {
+            return instance
+        }
+        instance = this
+
+        //global access
+        window.experience = this
+
+        // options
+        this.canvas = canvas
+        
+        // setup
+        this.debug = new Debug()
+        this.sizes = new Sizes()
+        this.time = new Time()
+        this.scene = new THREE.Scene()
+        this.resources = new Resources(sources)
+        this.camera = new Camera()
+        this.renderer = new Renderer()
+        this.world = new World()
+
+        // sizes resize event
+        this.sizes.on('resize', () =>
+        {
+            this.resize()
+        })
+
+        // time tick event
+        this.time.on('tick', () =>
+        {
+            this.update()
+        })
+    }
+    resize()
+    {
+        // console.log('A resize occured')
+        this.camera.resize()
+    }
+
+    update()
+    {
+        this.camera.update()
+        // console.log('update the experience') 
+        this.world.update()
+        this.renderer.update()
+    }
+
+    destroy()
+    {
+        this.sizes.off('resize')
+        this.time.off('tick')
+
+        //traverse the whole scene
+        this.scene.traverse((child) =>
+        {
+            if(child instanceof THREE.Mesh)
+            {
+                // console.log(child)
+                child.geometry.dispose()
+
+                for(const key in child.material)
+                {
+                    const value = child.material[key]
+                    // console.log(value)
+
+                    if(value && typeof value.dispose === 'function')
+                    {
+                        value.dispose()
+                    }
+                }
+            }
+        })
+
+        this.camera.controls.dispose()
+        this.renderer.instance.dispose()
+
+        if(this.debug.active)
+            this.debug.ui.destroy()
+    }
+
+}
